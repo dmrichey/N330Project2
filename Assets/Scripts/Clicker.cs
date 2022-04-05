8d6e1a7d535ff75993cac40870f2d6aa63fcd5f6
slidchickenleg <73666646+slidchickenleg@users.noreply.github.com>
2022-04-05 11:48:38 -0500

    Slow progress, but progress

--- Assets/Scripts/Clicker.cs
+++ Assets/Scripts/Clicker.cs
@@ -7,29 +7,42 @@ public class Clicker : MonoBehaviour
 {
 	bool active = false;
 	GameObject note;
+	private string previous="Note";
 
 	GameManager gm;
-	//public int points;
-	//public Text pointsText;
 	
 	void Start ()
 	{
 		gm = GameObject.Find("_GameManager").GetComponent<GameManager>();
 	}	
+	
+	// if it is a normaL blue note and the previous one is a normal note then it can just be clicked
+	// if the previous was a hold note, then the hold note has to be in the hitbox at the same time the note is for it to count, will be 2 points so do score() twice.
+	// if it is a holdnote and the previous one is normal, then it needs to be grabbed, dont do score just make it draggable.
+	// if the previous was a hold note, then they both go away score() twice.
+	
 	void OnMouseDown() {
 		if(active && note.tag=="Note") {
 			Destroy(note);
-			//points++;
-			//setPointText();
 			gm.Score();
+			gm.setPrevious("Note");
+			
 		}
-	}            
+		else if(active && note.tag =="HoldNote"){
+			gm.setPrevious("HoldNote");
+		}
+	}  
+	
+	/* when a new note enters any of the 4 hitboxes,
+	it updates with the previous note clicked from the gm */
 	public void OnTriggerEnter2D(Collider2D collision){
 		if(collision.gameObject.tag == "Note"){
+			previous = gm.getPrevious();
 			note = collision.gameObject;
 			active = true;
 		}
 		if(collision.gameObject.tag == "HoldNote"){
+			previous = gm.getPrevious();
 			note = collision.gameObject;
 			active = true;
 		}
